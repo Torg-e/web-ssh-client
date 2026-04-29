7d94acc35ab57104d0c3c9ce7158ec3aae1724d4
Torge <113136657+Torg-e@users.noreply.github.com>
2026-04-29 10:34:58 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,20 +1,9 @@
 #!/bin/sh
 set -e
 
-if [ ! -f /app/cert.pem ]; then
-    openssl req -x509 -newkey rsa:4096 \
-        -keyout /app/key.pem \
-        -out /app/cert.pem \
-        -days 365 \
-        -nodes \
-        -subj "/CN=localhost"
-fi
-
 python -c "from cryptography.fernet import Fernet; Fernet('$VAULT_ENCRYPTION_KEY')"
 
 exec gunicorn \
-    --certfile=/app/cert.pem \
-    --keyfile=/app/key.pem \
     -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker \
     -b 0.0.0.0:5000 \
     -w 1 \
